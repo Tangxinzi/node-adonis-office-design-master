9c7fd86a3dbc80a2dc65b237914b6c2098968187
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2024-02-25 23:21:22 +0800

    PMS #4 办公空间需求统计

--- app/Controllers/Http/land/pms/IndexController.ts
+++ app/Controllers/Http/land/pms/IndexController.ts
@@ -7,7 +7,8 @@ import { v4 as uuidv4 } from 'uuid';
 export default class IndexController {
   public async index({ request, response, view, session }: HttpContextContract) {
     try {
-      const all = request.all(), products = await Database.from('land_products').andWhereNull('deleted_at').orderBy('created_at', 'desc').forPage(request.input('page', 1), 20)
+      const all = request.all()
+      const products = await Database.from('land_products').andWhereNull('deleted_at').orderBy('created_at', 'desc').forPage(request.input('page', 1), 20)
       return view.render('land/pms/index/index', {
         data: {
           title: '项目管理',
@@ -66,6 +67,40 @@ export default class IndexController {
         session.flash('message', { type: 'success', header: '更新成功', message: `${ all.name }项目信息已更新。` })
       }
 
+      if (all.button == 'OfficeSpaceDemandStatistics') {
+        console.log(all);
+
+        await Database.from('land_products_osds').where('product_id', all.product_id).update({
+          number_1: all.number_1,
+          number_2: all.number_2,
+          number_3: all.number_3,
+          number_4: all.number_4,
+          number_5: all.number_5,
+          number_6: all.number_6,
+          number_7: all.number_7,
+          number_8: all.number_8,
+          number_9: all.number_9,
+          number_10: all.number_10,
+          number_11: all.number_11,
+          number_12: all.number_12,
+          number_13: all.number_13,
+          number_14: all.number_14,
+          number_15: all.number_15,
+          number_16: all.number_16,
+          number_17: all.number_17,
+          number_18: all.number_18,
+          number_19: all.number_19,
+          number_20: all.number_20,
+          number_21: all.number_21,
+          number_22: all.number_22,
+          number_23: all.number_23,
+          number_24: all.number_24,
+          number_25: all.number_25,
+          number_26: all.number_26,
+          number_27: all.number_27,
+        })
+      }
+
       return response.redirect('back')
     } catch (error) {
       console.log(error)
@@ -76,7 +111,9 @@ export default class IndexController {
     try {
       let all = request.all(), dataset = {
         information_documents: []
-      }, product = await Database.from('land_products').where('product_id', params.id).andWhereNull('deleted_at').first()
+      }
+      const product = await Database.from('land_products').where('product_id', params.id).andWhereNull('deleted_at').first() || {}
+      const land_products_osds = await Database.from('land_products_osds').where('product_id', params.id).andWhereNull('deleted_at').first() || {}
 
       switch (params.step) {
         case 'step-04':
@@ -141,6 +178,7 @@ export default class IndexController {
           all,
           step: params.step,
           product,
+          land_products_osds,
           ...dataset
         }
       })
